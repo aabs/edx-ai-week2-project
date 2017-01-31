167d7163877a8de3c6c0974787fc1793dc8d74a0
Andrew Matthews <matthews.andrew@gmail.com>
2017-01-31 22:21:08 +1100

    another order of mag perf improvement

--- driver.py
+++ driver.py
@@ -9,8 +9,8 @@ import unittest
 profile_the_code = False
 output_to_console = True
 progress_log_granularity = 10000
-should_log_progress = False
-log_level = 3
+should_log_progress = True
+log_level = 2
 
 def logit(method):
     def timed(*args, **kw):
@@ -29,21 +29,25 @@ def log(msg, level=0):
 
 
 def dispatch_command(command, board_layout):
-    if command == "bfs":
-        bfs = BreadthFirstSearch(board_layout)
-        return bfs.search()
-    elif command == "dfs":
-        dfs = DepthFirstSearch(board_layout)
-        return dfs.search()
-    elif command == "ast":
-        ast = AStarSearch(board_layout)
-        return ast.search()
-    elif command == "ida":
-        ida = IDAStarSearch(board_layout)
-        return ida.search()
-    else:
-        display_usage(command)
+    if profile_the_code:
+        pr = cProfile.Profile()
+        pr.enable()
 
+    searcher = None
+    searcher = BreadthFirstSearch(board_layout) if command == "bfs" else searcher
+    searcher = DepthFirstSearch(board_layout) if command == "dfs" else searcher
+    searcher = AStarSearch(board_layout) if command == "ast" else searcher
+    searcher = IDAStarSearch(board_layout) if command == "ida" else searcher
+    if searcher is None:
+        display_usage(command)
+        return 1
+    result = searcher.search()
+
+    if profile_the_code:
+        pr.disable()
+        # after your program ends
+        pr.print_stats(sort="tottime")
+    return result
 
 def display_usage(command):
     """please invoke like this: python driver.py <method> <board>, where board is a comma separated list of digits from 0 to 8"""
@@ -57,43 +61,48 @@ def swap_list_elements(l, from_idx, to_idx):
     l[to_idx] = 0
     l[from_idx] = tmp
 
+def parse_layout(layout_as_string):
+    """convert a comma separated list of chars into a vector"""
+    # check if list is already a list of integers (in which case just return a cloned copy of it)
+    if all(isinstance(x, int) for x in layout_as_string):
+        return list(layout_as_string)
+
+    list_of_strings = layout_as_string.split(",")
+    result = []
+    for s in list_of_strings:
+        result.append(int(s))
+        # TODO: write code...
+    return result
+
 
 def contains(target, seq1):
-    for item in seq1:
-        if target.board_layout._layout_hash == item.board_layout._layout_hash:
-            return True
-    return False
+    return target in seq1
+    # for item in seq1:
+    #     if target.board_layout._layout_hash == item.board_layout._layout_hash:
+    #         return True
+    # return False
 
 
 class BoardLayout():
     """a class representing an instance of a configuration of the board"""
 
-    def parse_layout_to_vector(self, layout):
-        """convert a comma separated list of chars into a vector"""
-        # check if list is already a list of integers (in which case just return a cloned copy of it)
-        if all(isinstance(x, int) for x in layout):
-            return list(layout)
-
-        list_of_strings = layout.split(",")
-        result = []
-        for s in list_of_strings:
-            result.append(int(s))
-            # TODO: write code...
-        return result
-
-    def __init__(self, layout, expectwellformed=False):
+    def __init__(self, layout):
         """initialise from a comma separated list of characters"""
-        if expectwellformed:
-            self.state = layout
-        else:
-            self.state = self.parse_layout_to_vector(layout)
+        self.state = layout
         x = math.trunc(math.sqrt(len(self.state)))
         # see whether the board is square
         if (x * x) != len(self.state):
             raise Exception("board is not square!")
         self.boardWidth = x
         self.boardHeight = x
-        self._layout_hash = str(self.state).__hash__()
+        self._layout_hash = self.conv_layout_to_dense_int()
+
+    def conv_layout_to_dense_int(self):
+        result = 0
+        for num in self.state:
+            result *= 10
+            result += num
+        return result
 
     def availableMoves(self):
         """Return a list of available moves (order is UDLR)"""
@@ -122,21 +131,24 @@ class BoardLayout():
             raise Exception("Invalid move!")
         # so, it's safe to move, so clone a copy of the board to make the move on
         newBoard = list(self.state)
-        indexOfBlankSpace = newBoard.index(0)  # find location of blank space
+        to_idx = from_idx = newBoard.index(0)  # find location of blank space
         if move == "Up":
-            swap_list_elements(newBoard, indexOfBlankSpace, indexOfBlankSpace - self.boardWidth)
+            to_idx= from_idx - self.boardWidth
         if move == "Down":
-            swap_list_elements(newBoard, indexOfBlankSpace, indexOfBlankSpace + self.boardWidth)
+            to_idx = from_idx + self.boardWidth
         if move == "Left":
-            swap_list_elements(newBoard, indexOfBlankSpace, indexOfBlankSpace - 1)
+            to_idx = from_idx - 1
         if move == "Right":
-            swap_list_elements(newBoard, indexOfBlankSpace, indexOfBlankSpace + 1)
+            to_idx = from_idx + 1
 
-        result = BoardLayout(newBoard, True)
+        newBoard[from_idx],newBoard[to_idx] = newBoard[to_idx],newBoard[from_idx]
+        # swap_list_elements(newBoard, from_idx, to_idx)
+        result = BoardLayout(newBoard)
         return result
 
     def layout_is_acceptable(self):
-        return all(self.state[i] <= self.state[i + 1] for i in range(len(self.state) - 1))
+        return self._layout_hash == 12345678
+        # return all(self.state[i] <= self.state[i + 1] for i in range(len(self.state) - 1))
 
     def __hash__(self):
         return self._layout_hash
@@ -165,7 +177,14 @@ class StateSpaceElement():
             return self.index_key
         self.index_key = str(self.board_layout)
         return self.index_key
-
+    @property
+    def search_depth(self):
+        layers = 1
+        x = self.parent_layout
+        while x is not None:
+            layers += 1
+            x = x.parent_layout
+        return layers
     def __hash__(self):
         return self.board_layout.__hash__()
     def __eq__(self, other):
@@ -228,7 +247,7 @@ class SearchAlgorithm():
     def update_max_fringe_size(self):
         self.max_fringe_size = max(self.max_fringe_size, len(self.fringe))
         if self.max_fringe_size % 500 == 0:
-            log(" %d"%self.max_fringe_size, 2)
+            log(" %d"%self.max_fringe_size, 1)
 
 
 class BreadthFirstSearch(SearchAlgorithm):
@@ -248,12 +267,13 @@ class BreadthFirstSearch(SearchAlgorithm):
             if state.board_layout_is_acceptable():
                 return self.success(state)
 
-            self.explored[state.board_layout._layout_hash] = state
-            self.max_search_depth = max(self.max_search_depth, len(self.get_path_to_goal(state)))
+            self.explored[hash(state.board_layout)] = state
+            # self.max_search_depth = max(self.max_search_depth, len(self.get_path_to_goal(state)))
+            self.max_search_depth = max(self.max_search_depth, state.search_depth)
 
             for neighbour in self.expand_node(state):
-                tmp1 = self.explored.get(neighbour.board_layout._layout_hash, None)
-                if not contains(neighbour, fr) and tmp1 is None:
+                tmp1 = self.explored.get(hash(neighbour.board_layout), None)
+                if neighbour not in fr and tmp1 is None:
                     app(neighbour)
 
             self.update_max_fringe_size()
@@ -261,15 +281,15 @@ class BreadthFirstSearch(SearchAlgorithm):
 
 
 class DepthFirstSearch(SearchAlgorithm):
-    def __init__(self, startingBoardLayout):
-        """Initailises the runtime state. Uses a list (as a stack) for the fringe, and a set for the explored set."""
-        self.startLayout = startingBoardLayout
+    def __init__(self, starting_board_layout):
+        """Initialises the runtime state. Uses a list (as a stack) for the fringe, and a set for the explored set."""
+        self.start_layout = starting_board_layout
         SearchAlgorithm.__init__(self, [], dict())
 
     def search(self):
         fr = self.fringe
         app = fr.append
-        app(StateSpaceElement(self.startLayout, None, None))
+        app(StateSpaceElement(self.start_layout, None, None))
         self.update_max_fringe_size()
         explored_counter = 0
         while len(fr) > 0:
@@ -283,7 +303,7 @@ class DepthFirstSearch(SearchAlgorithm):
 
             if should_log_progress and (explored_counter % progress_log_granularity) == 0:
                 log("explored: %d"%explored_counter, 2)
-            self.max_search_depth = max(self.max_search_depth, len(self.get_path_to_goal(state)))
+            self.max_search_depth = max(self.max_search_depth, state.search_depth)
 
             child_nodes = self.expand_node(state)
             if len(child_nodes) > 0:
@@ -317,7 +337,7 @@ class AStarSearch(SearchAlgorithm):
             self.max_search_depth = max(self.max_search_depth, len(self.get_path_to_goal(state)))
 
             for neighbour in self.expand_node(state):
-                if not contains(neighbour, self.fringe) and not contains(neighbour, self.explored):
+                if neighbour not in self.fringe and neighbour not in self.explored:
                     self.fringe.append(neighbour)
 
             self.update_max_fringe_size()
@@ -352,21 +372,14 @@ class IDAStarSearch(SearchAlgorithm):
 
 
 def main():
-    startingBoardLayout = BoardLayout(sys.argv[2])
+    board_layout = parse_layout(sys.argv[2])
+    startingBoardLayout = BoardLayout(board_layout)
     return dispatch_command(sys.argv[1], startingBoardLayout)
 
 
 start_time = time.time()
 if __name__ == "__main__":
-    if profile_the_code:
-        pr = cProfile.Profile()
-        pr.enable()
-        main()
-        pr.disable()
-        # after your program ends
-        pr.print_stats(sort="tottime")
-    else:
-        main()
+    main()
 
 # a nice 7 step solution: bfs 1,2,5,0,4,8,3,6,7
 # path_to_goal: ['Down', 'Right', 'Right', 'Up', 'Up', 'Left', 'Left']
@@ -382,7 +395,8 @@ if __name__ == "__main__":
 
 class SearchTests(unittest.TestCase):
     def test0_7_step_solution(self):
-        startingBoardLayout = BoardLayout("1,2,5,0,4,8,3,6,7")
+        layout = parse_layout("1,2,5,0,4,8,3,6,7")
+        startingBoardLayout = BoardLayout(layout)
         (searcher, finalState) = dispatch_command("bfs", startingBoardLayout)
         path = searcher.get_path_to_goal(finalState)
         self.assertEqual(path, ['Down', 'Right', 'Right', 'Up', 'Up', 'Left', 'Left'])
@@ -400,7 +414,8 @@ class SearchTests(unittest.TestCase):
         self.assertEqual(len(searcher.explored), 1)
 
     def test1_1_bfs(self):#bfs 1,2,5,3,4,0,6,7,8
-        startingBoardLayout = BoardLayout("1,2,5,3,4,0,6,7,8")
+        layout = parse_layout("1,2,5,3,4,0,6,7,8")
+        startingBoardLayout = BoardLayout(layout)
         (searcher, finalState) = dispatch_command("bfs", startingBoardLayout)
         path = searcher.get_path_to_goal(finalState)
         self.assertEqual(path, ['Up', 'Left', 'Left'])
@@ -409,7 +424,8 @@ class SearchTests(unittest.TestCase):
         self.assertEqual(len(searcher.explored), 10)
 
     def test1_2_dfs(self):#dfs 1,2,5,3,4,0,6,7,8
-        startingBoardLayout = BoardLayout("1,2,5,3,4,0,6,7,8")
+        layout = parse_layout("1,2,5,3,4,0,6,7,8")
+        startingBoardLayout = BoardLayout(layout)
         (searcher, finalState) = dispatch_command("dfs", startingBoardLayout)
         path = searcher.get_path_to_goal(finalState)
         self.assertEqual(path, ['Up', 'Left', 'Left'])
